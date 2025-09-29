12546dc500b5df1fe78402a435fdd46572516743
wszala <wojnad@o2.pl>
2025-09-29 12:38:21 +0200

    Imrovements

--- src/main/java/com/aisupport/util/AIConnection.java
+++ src/main/java/com/aisupport/util/AIConnection.java
@@ -5,38 +5,48 @@ import com.aisupport.service.AIService;
 import com.aisupport.service.AIServiceFactory;
 
 public class AIConnection {
+
+    private static final String OLLAMA_ENGINE_NAME = "ollama";
+    private static final String VLLM_ENGINE_NAME = "vllm";
+    private static final String OLLAMA_DEFAULT_MODEL = "gemma2";
+    private static final String VLLM_DEFAULT_MODEL = "mistral_7b_it";
+    private static final String OLLAMA_URL = "http://20.185.83.16:8080/";
+    private static final String VLLM_URL = "http://9.169.65.166:8080/v1";
+    private static final String OLLAMA_API_KEY = "aie93JaTv1GW1AP4IIUSqeecV22HgpcQ6WlgWNyfx2HflkY5hTw19JDbT90ViKcZaZ6lpjOo3YIGgpkG7Zb8jEKvdM5Ymnq9jPm79osLppCebwJ7WdWTwWq3Rf15NDxm";
+    private static final String VLLM_API_KEY = "dummy";
+
     public static AIService provideService(AIBasicConfig config) {
         return AIServiceFactory.createService(config); // Placeholder for actual implementation
     }
 
     public static AIService provideOllamaGemma2Service(){
-        AIBasicConfig configOllama = new AIBasicConfig("ollama",
-                "aie93JaTv1GW1AP4IIUSqeecV22HgpcQ6WlgWNyfx2HflkY5hTw19JDbT90ViKcZaZ6lpjOo3YIGgpkG7Zb8jEKvdM5Ymnq9jPm79osLppCebwJ7WdWTwWq3Rf15NDxm",
-                "http://20.185.83.16:8080/",
-                "gemma2", 2048, 0.7);
+        AIBasicConfig configOllama = new AIBasicConfig(OLLAMA_ENGINE_NAME,
+                OLLAMA_API_KEY,
+                OLLAMA_URL,
+                OLLAMA_DEFAULT_MODEL, 2048, 0.7);
         return AIConnection.provideService(configOllama);
     }
 
-    public static AIService provideVllmLlama38bService(){
-        AIBasicConfig config = new AIBasicConfig("vllm",
-                "dummy",
-                "http://9.169.65.166:8080/v1",
-                "llama3_8b_it", 2048, 0.7);
+    public static AIService provideVllmBasicService(){
+        AIBasicConfig config = new AIBasicConfig(VLLM_ENGINE_NAME,
+                VLLM_API_KEY,
+                VLLM_URL,
+                VLLM_DEFAULT_MODEL, 2048, 0.7);
         return AIConnection.provideService(config);
     }
 
     public static AIService provideOllamaService(String model){
-        AIBasicConfig configOllama = new AIBasicConfig("ollama",
-                "aie93JaTv1GW1AP4IIUSqeecV22HgpcQ6WlgWNyfx2HflkY5hTw19JDbT90ViKcZaZ6lpjOo3YIGgpkG7Zb8jEKvdM5Ymnq9jPm79osLppCebwJ7WdWTwWq3Rf15NDxm",
-                "http://20.185.83.16:8080/",
+        AIBasicConfig configOllama = new AIBasicConfig(OLLAMA_ENGINE_NAME,
+                OLLAMA_API_KEY,
+                OLLAMA_URL,
                 model, 2048, 0.7);
         return AIConnection.provideService(configOllama);
     }
 
     public static AIService provideVllmService(String model){
-        AIBasicConfig configVllm = new AIBasicConfig("vllm",
-                "dummy",
-                "http://9.169.65.166:8080/v1",
+        AIBasicConfig configVllm = new AIBasicConfig(VLLM_ENGINE_NAME,
+                VLLM_API_KEY,
+                VLLM_URL,
                 model, 2048, 0.7);
         return AIConnection.provideService(configVllm);
     }
