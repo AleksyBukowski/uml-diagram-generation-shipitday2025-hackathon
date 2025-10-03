e0ce52952bef4e35c650d9c738a857bd5645f9b7
AleksyBukowski <aleksybukowski04@gmail.com>
2025-10-03 13:11:18 +0200

    Initial

--- src/main/java/com/aisupport/Main.java
+++ src/main/java/com/aisupport/Main.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+        // Load configuration and create service
+        AIConfig config = new AIConfig();
+        AIService aiService = AIServiceFactory.createService(config);
 
+        // Send a message
+        String response = aiService.chat("What is the capital of France?");
+        System.out.println(response);
     }
 }
